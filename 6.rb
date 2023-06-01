230b5cd9e4762a7b8c03553d5d13c2643afe1711
charlie51417 <charlie51417@gmail.com>
2023-06-01 14:54:47 +0800

    Ruby: Question Candy6

---
+++ 6.rb
@@ -0,0 +1,15 @@
+# 編號：CANDY-006
+# 程式語言：Ruby
+# 題目：找出在數字陣列裡跟其它元素不一樣的值
+
+def findDifferent(numbers) 
+  numbers.select{ |n| numbers.count(n) == 1 }.first
+end
+
+  
+    
+p findDifferent([1, 1, 1, 1, 3, 1, 1, 1]) # 印出 3
+p findDifferent([2, 2, 2, 4, 2, 2]) # 印出 4
+p findDifferent([8, 3, 3, 3, 3, 3, 3, 3]) # 印出 8
+
+    
