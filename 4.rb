0d9da8c504ddd51969eacc82789faed8541424b9
charlie51417 <charlie51417@gmail.com>
2023-06-01 13:23:36 +0800

    Ruby: Question Candy4

--- 4.rb
+++ 4.rb
@@ -1,28 +1,22 @@
 # 編號：CANDY-004
-# 程式語言：JavaScript
+# 程式語言：Ruby
 # 題目：完成函數的內容，把傳進去的秒數變成平常人類看的懂的時間格式
 
 def humanReadableTimer(seconds)
-
+  hours = seconds/3600
+  mins = (seconds/60) % 60
+  sec = seconds % 60
+  return "#{hours.to_s.rjust(2, '0')}" + ":" + "#{mins.to_s.rjust(2, '0')}" + ":" + "#{sec.to_s.rjust(2, '0')}"
 end
-function humanReadableTimer(seconds) {
-    const sec = seconds % 60
-    const mins = Math.floor(seconds / 60) % 60;
-    const hours = Math.floor(seconds / 3600) % 100;
-    let TenDigitSec = (sec < 10) ? "0" : "";
-    let TenDigitMin = (mins < 10) ? "0" : "";
-    let TenDigitHour = (hours < 10) ? "0" : "";
+  p humanReadableTimer(0) # 印出 00:00:00
+  p humanReadableTimer(59) # 印出 00:00:59
+  p humanReadableTimer(60) # 印出 00:01:00
+  p humanReadableTimer(90) # 印出 00:01:30
+  p humanReadableTimer(3599) # 印出 00:59:59
+  p humanReadableTimer(3600) # 印出 01:00:00
+  p humanReadableTimer(45296) # 印出 12:34:56
+  p humanReadableTimer(86399) # 印出 23:59:59
+  p humanReadableTimer(86400) # 印出 24:00:00
+  p humanReadableTimer(359999) # 印出 99:59:59
 
-    return (TenDigitHour+ hours + ":"+ TenDigitMin + mins + ":"+ TenDigitSec + sec);
-  }
   
-  console.log(humanReadableTimer(0)) // 印出 00:00:00
-  console.log(humanReadableTimer(59)) // 印出 00:00:59
-  console.log(humanReadableTimer(60)) // 印出 00:01:00
-  console.log(humanReadableTimer(90)) // 印出 00:01:30
-  console.log(humanReadableTimer(3599)) // 印出 00:59:59
-  console.log(humanReadableTimer(3600)) // 印出 01:00:00
-  console.log(humanReadableTimer(45296)) // 印出 12:34:56
-  console.log(humanReadableTimer(86399)) // 印出 23:59:59
-  console.log(humanReadableTimer(86400)) // 印出 24:00:00
-  console.log(humanReadableTimer(359999)) // 印出 99:59:59
